0efce8b25785aa69e0942996e16819370b1ee911
Bilalahmad366 <bm625416gg@gmail.com>
2024-07-22 22:24:59 -0700

    second

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/https://github.com/Bilalahmad366/rent-collection-system-/',
+  base:'/https://github.com/Bilalahmad366/rent-collection-system/',
   plugins: [
     vue(),
   ],
